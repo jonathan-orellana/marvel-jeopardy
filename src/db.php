7f94e60e65eb5df490986f57a8e98e548264ea29
Jonathan Orellana <qrk9cs@virginia.edu>
2025-11-24 21:38:46 -0500

    fix database connection

--- src/db.php
+++ src/db.php
@@ -4,14 +4,25 @@ require __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
 $dotenv->load();
 
-$db = pg_connect(sprintf(
+$db = @pg_connect(sprintf(
     "host=%s port=%s dbname=%s user=%s password=%s",
-    $_ENV['LOCAL_DB_HOST'],
-    $_ENV['LOCAL_DB_PORT'],
-    $_ENV['LOCAL_DB_NAME'],
-    $_ENV['LOCAL_DB_USER'],
-    $_ENV['LOCAL_DB_PASSWORD']
+    $_ENV['SERVER_DB_HOST'],
+    $_ENV['SERVER_DB_PORT'],
+    $_ENV['SERVER_DB_NAME'],
+    $_ENV['SERVER_DB_USER'],
+    $_ENV['SERVER_DB_PASSWORD']
 ));
+
+if (!$db) {
+    $db = @pg_connect(sprintf(
+        "host=%s port=%s dbname=%s user=%s password=%s",
+        $_ENV['LOCAL_DB_HOST'],
+        $_ENV['LOCAL_DB_PORT'],
+        $_ENV['LOCAL_DB_NAME'],
+        $_ENV['LOCAL_DB_USER'],
+        $_ENV['LOCAL_DB_PASSWORD']
+    ));
+}
 
 if (!$db) {
     die("DB connection failed.");
